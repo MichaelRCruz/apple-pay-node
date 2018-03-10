c1ec07d35c52855f3acf573ea421ff3f63121ebf
Michael Cruz <michael.cruz@uwalumni.com>
2018-03-09 19:25:53 -0800

    have a model

---
+++ models.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const paymentSchema = mongoose.Schema({
+  // waiting to see what apple pay API does
+});
+
+// virtuals reminder
+paymentSchema.virtual('addressString').get(function() {
+  return `${this.address.building} ${this.address.street}`.trim()});
+
+restaurantSchema.virtual('grade').get(function() {
+  const gradeObj = this.grades.sort((a, b) => {return b.date - a.date})[0] || {};
+  return gradeObj.grade;
+});
+
+paymentSchema.methods.serialize = function() {
+  return {
+    // not sure yet
+  };
+}
+
+const Payment = mongoose.model('Payment', paymentSchema);
+
+module.exports = {Payment};

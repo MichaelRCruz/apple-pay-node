928d8dd5a4be085e02fab365c7ae204b84bfe77f
Michael Cruz <michael.cruz@uwalumni.com>
2018-03-09 19:16:08 -0800

    build routes

---
+++ server.js
@@ -0,0 +1,78 @@
+'use strict';
+
+const bodyParser = require('body-parser');
+const express = require('express');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+const { PORT, DATABASE_URL } = require('./config');
+const { Payment } = require('./models');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.get('/payments', (req, res) => {
+  console.log('Hello, payments.');
+});
+
+app.get('/payments/:id', (req, res) => {
+  console.log('Hello, payment.');
+});
+
+app.post('/payments', (req, res) => {
+  console.log('Hello, payment post.');
+});
+
+app.put('/payments/:id', (req, res) => {
+  console.log('Hello, paymemnts put.');
+});
+
+app.delete('/payments/:id', (req, res) => {
+  console.log('Goodbye, payment.');
+});
+
+app.use('*', function (req, res) {
+  res.status(404).json({ message: 'Not Found' });
+});
+
+let server;
+
+function runServer(databaseUrl, port = PORT) {
+
+  return new Promise((resolve, reject) => {
+    mongoose.connect(databaseUrl, err => {
+      if (err) {
+        return reject(err);
+      }
+      server = app.listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve();
+      })
+        .on('error', err => {
+          mongoose.disconnect();
+          reject(err);
+        });
+    });
+  });
+}
+
+function closeServer() {
+  return mongoose.disconnect().then(() => {
+    return new Promise((resolve, reject) => {
+      console.log('Closing server');
+      server.close(err => {
+        if (err) {
+          return reject(err);
+        }
+        resolve();
+      });
+    });
+  });
+}
+
+if (require.main === module) {
+  runServer(DATABASE_URL).catch(err => console.error(err));
+}
+
+module.exports = { app, runServer, closeServer };

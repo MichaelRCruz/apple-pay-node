258eea06f5dfb29d201c71beed4c590ecd6b4394
Michael Cruz <mcruz@iclpt00103.local>
2018-03-20 13:09:30 -0700

    configure database and auth

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 'use strict';
-// exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/apple-pay-node';
-// exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-apple-pay-node';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/doc-log-dev';
 exports.PORT = process.env.PORT || 8080;
+exports.JWT_SECRET = process.env.JWT_SECRET;
+exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';

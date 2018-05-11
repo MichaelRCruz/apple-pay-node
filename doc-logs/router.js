2599df020256f1e1d8ba730a9bd302ba6f525af7
Michael Cruz <michael@michaelcruz.io>
2018-05-10 21:32:04 -0700

    add dom id intended for slugs

--- doc-logs/router.js
+++ doc-logs/router.js
@@ -4,6 +4,7 @@ const dummyLog = [
   {
     component_type: 'app-text-title',
     content: "An Introduction to DocLog and Why it's useful",
+    dom_id: '11111111111',
     title_metadata: {
       date: 'March 19, 2017',
       view_count: '1.2'
@@ -11,11 +12,13 @@ const dummyLog = [
   },
   {
     component_type: 'app-text-blob',
+    dom_id: '22222222222',
     subtitle: 'Introduction',
     content: "Lorem ipsum dolor amet dIY gluten-free hell of tumblr vaporware tilde XOXO photo booth mixtape quinoa VHS offal. Tattooed waistcoat helvetica literally hammock, deep v raw denim man braid tumblr. Jean shorts thundercats mlkshk, skateboard enamel pin cold-pressed man bun lomo locavore XOXO. \n \n Biodiesel fingerstache ennui mustache twee pork belly brunch tattooed intelligentsia pitchfork whatever four dollar toast VHS [I'm an inline-style link](https://www.google.com) church-key sustainable. Beard portland affogato green juice shabby chic, kale chips copper mug dreamcatcher gastropub snackwave taxidermy subway tile. Tacos four loko kitsch, freegan thundercats deep v palo santo 8-bit cliche offal copper mug ugh fingerstache. Fam selvage vinyl literally jianbing taiyaki."
   },
   {
     component_type: 'app-code-snippet',
+    dom_id: '33333333333',
     language: 'javascript',
     content: `router.get('/', (req, res) => {
     return res.json(dummyLog);
@@ -23,11 +26,13 @@ const dummyLog = [
   },
   {
     component_type: 'app-code-snippet',
+    dom_id: '44444444444',
     language: 'bash',
     content: `npm i moment`
   },
   {
     component_type: 'app-code-snippet',
+    dom_id: '55555555555',
     language: 'python',
     content: `def happyBirthdayEmily(): #program does nothing as written
     print("Happy Birthday to you!")
@@ -38,6 +43,7 @@ const dummyLog = [
   },
   {
     component_type: 'app-text-blob',
+    dom_id: '66666666666',
     subtitle: 'Let\'s Take a Look',
     content: `Lorem ipsum dolor amet dIY gluten-free hell of tumblr vaporware tilde XOXO photo booth mixtape quinoa VHS offal. Tattooed waistcoat helvetica literally hammock, deep v raw denim man braid tumblr. Jean shorts thundercats mlkshk, skateboard enamel pin cold-pressed man bun lomo locavore XOXO.
 
@@ -51,10 +57,12 @@ chips copper mug dreamcatcher gastropub snackwave taxidermy subway tile. Tacos f
   },
   {
     component_type: 'app-display-image',
+    dom_id: '77777777777',
     content: 'https://i.imgur.com/0FB6LAi.png'
   },
   {
     component_type: 'app-text-blob',
+    dom_id: '88888888888',
     subtitle: 'Let\'s Take a Look',
     content: "Lorem ipsum dolor amet dIY gluten-free hell of tumblr vaporware tilde XOXO photo booth mixtape quinoa VHS offal. Tattooed waistcoat helvetica literally hammock, deep v raw denim man braid tumblr. Jean shorts thundercats mlkshk, skateboard enamel pin cold-pressed man bun lomo locavore XOXO. Biodiesel fingerstache ennui mustache twee pork belly brunch tattooed intelligentsia pitchfork whatever four dollar toast VHS church-key sustainable. Beard portland affogato green juice shabby chic, kale chips copper mug dreamcatcher gastropub snackwave taxidermy subway tile. Tacos four loko kitsch, freegan thundercats deep v palo santo 8-bit cliche offal copper mug ugh fingerstache. Fam selvage vinyl literally jianbing taiyaki."
   }
